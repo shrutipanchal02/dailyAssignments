529345ce1268041dec58136b13dfa9f7a561bd10
shrutipanchal02 <shruti.panchal@vmvinfotech.com>
2025-01-01 12:42:12 +0530

    Add files via upload

---
+++ java_program/array2d.java
@@ -0,0 +1,11 @@
+public class array2d{
+public static void main(String[] args){
+int[][] a={{10,20,30},{10,20,30}};
+for(int i=0;i<a.length;i++){
+for(int j=0;j<a[i].length;j++){
+System.out.print(a[i][j] +" ");
+}
+System.out.println(" ");
+}
+}
+}
